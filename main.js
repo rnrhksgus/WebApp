7c8d659c957b3f6b113c5649f9239a698d4eafe1
rnrhksgus <kkh7574z@naver.com>
2019-02-22 02:05:11 +0900

    css

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#menu').hover(function(){
+        $('#sub_menu').css('visibility', 'visible');
+    }, function(){
+        $('#sub_menu').css('visibility', 'hidden');
+    });
+});
