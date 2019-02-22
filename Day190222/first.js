59bf1832478f32256801481da5d5a674e5a11e24
rnrhksgus <kkh7574@naver.com>
2019-02-22 14:14:15 +0900

    12

--- Day190222/first.js
+++ Day190222/first.js
@@ -14,3 +14,49 @@ console.log("x = " + x);
 
 //x = 100;
 console.log("x = " + x);
+
+console.log();
+console.log("---------------------------------");
+console.log();
+
+var count = 10;
+var blue = 0x0000ff;
+var umask = 0o0022;
+var roomTemp = 21.5;
+var c = 3.0e6
+var e = -1.6e-19;
+var inf = Infinity;
+var ninf = -Infinity;
+var nan = NaN;
+
+console.log(count);
+console.log(blue);
+console.log(umask);
+console.log(roomTemp);
+console.log(c);
+console.log(e);
+console.log(inf);
+console.log(ninf);
+console.log(nan);
+
+console.log();
+console.log("---------------------------------");
+console.log();
+
+var small = Number.EPSILON;
+var bigInt = Number.MAX_SAFE_INTEGER;
+var max = Number.MAX_VALUE;
+var minInt = Number.MIN_SAFE_INTEGER;
+var min = Number.MIN_VALUE;
+var nInf = Number.NEGATIVE_INFINITY;
+var inf2 = Number.POSITIVE_INFINITY;
+var nan2 = Number.NaN;
+
+console.log(small);
+console.log(bigInt);
+console.log(max);
+console.log(minInt);
+console.log(min);
+console.log(nInf);
+console.log(inf2);
+console.log(nan2);
