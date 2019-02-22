0f74f9cfcb327d5578be8ca3c3f34e4e57dbf958
rnrhksgus <kkh7574@naver.com>
2019-02-22 13:42:37 +0900

    add

---
+++ Day190222/first.js
@@ -0,0 +1,16 @@
+var a;
+let b = 20;
+
+console.log("Hello, world!!!");
+console.log("b = " + b);
+console.log("a = " + a);
+console.log("a + b = " + (a + b));
+
+a = 30;
+console.log("a = " + a);
+
+const x = 10;
+console.log("x = " + x);
+
+//x = 100;
+console.log("x = " + x);
