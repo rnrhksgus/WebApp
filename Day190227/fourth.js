d793cd715a088a71b0d368f13f86cdd65cfd4131
rnrhksgus <kkh7574@naver.com>
2019-02-27 17:07:55 +0900

    ad

--- Day190227/fourth.js
+++ Day190227/fourth.js
@@ -304,3 +304,18 @@ console.log( alphaetical );
 let longWords = words.reduce( ( a, w ) => w.length > 6 ? a + " " + w : a, "").trim();
 console.log( longWords );
 console.log("------------------------------------------------------------------");
+
+// Donald Knuth이 작성한 분산 계산을 위한 알고리즘
+let data = [ 3.3, 5, 7.2, 12, 4, 6, 10.3 ];
+let stats = data.reduce( ( a, x ) => {
+    a.N++;
+    let delta = x - a.mean;
+    a.mean += delta / a.N;
+    return a;
+}, { N: 0, mean: 0, M2: 0 } );
+if ( stats.N > 2 ) {
+  stats.variance = stats.M2 / (stats.N - 1);
+  stats.stdev = Math.sqrt( stats.variance );
+}
+console.log( stats );
+console.log("------------------------------------------------------------------");
