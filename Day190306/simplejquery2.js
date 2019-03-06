73bfb1ba3e530804f0d3414cf24023869e371a3c
rnrhksgus <kkh7574@naver.com>
2019-03-06 18:18:08 +0900

    add

--- Day190306/simplejquery2.js
+++ Day190306/simplejquery2.js
@@ -1,4 +1,24 @@
 
 $(document).ready(function() {
-    
+
+});
+
+$( ".bdiv" ).click(function() {
+    var color = $( this ).css( "background-color" );
+    $( "#result" ).html( "That div is <span style='color:" + color + ";'>" + color + "</span>." );
+});
+
+$( "p" ).on( "mouseover", function() {
+  $( this ).css( "color", "red" );
+});
+
+$( ".cdiv" ).on( "click", function() {
+  $( this ).css({
+    width: function( index, value ) {
+      return parseFloat( value ) * 1.2;
+    },
+    height: function( index, value ) {
+      return parseFloat( value ) * 1.2;
+    }
+  });
 });
